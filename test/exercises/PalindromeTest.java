7ae7ce8d4ce85146fbbec1e4f787c9fc93fec2e0
IvanaDjikic <ivdjikic@gmail.com>
2019-07-15 01:46:13 +0200

    palindrometest change

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -5,16 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
-	
+
 	Palindrome testPalindrome = new Palindrome();
 
 	@Test
 	void testLoadWords() {
 		assertTrue(testPalindrome.loadWords().contains("apple"));
 	}
-	
-	// 2. Test that a word exists in the dictionary 
-	
+
+	// 2. Test that a word exists in the dictionary
+	@Test
+	void testWordIsInDictionary() {
+		assertTrue(testPalindrome.wordIsInDictionary("aback"));
+	}
+
 	// 3. Test that a word is a palindrome
+	@Test
+	void testReverseWord() {
+		// first way
+		assertEquals("radar", testPalindrome.reverseWord("radar"));
+		// second way
+		assertTrue(testPalindrome.isPalindrome("wow"));
+		assertFalse(testPalindrome.isPalindrome("green"));
+	}
 
 }
