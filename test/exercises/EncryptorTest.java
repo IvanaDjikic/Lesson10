d98a76829cb9e5065684aafd0c064add21d043da
IvanaDjikic <ivdjikic@gmail.com>
2019-07-15 01:43:34 +0200

    enctyptortest change

--- test/exercises/EncryptorTest.java
+++ test/exercises/EncryptorTest.java
@@ -16,8 +16,13 @@ class EncryptorTest {
 		assertEquals("ccc", Encryptor.encrypt("aaa"));
 		assertEquals("Lcxc", Encryptor.encrypt("Java"));
 	}
-	
+
 	// Exercise: decrypt this sentence: Nkxg"nqpi"cpf"rtqurgt#
-	
-	
+
+	@Test
+	void testDecryptWord() throws Exception {
+		assertEquals("Nkxg\"nqpi\"cpf\"rtqurgt#", Encryptor.encrypt("Live long and prosper!"));
+
+	}
+
 }
