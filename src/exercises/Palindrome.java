903c890db0e08cd953c116d53dc75747753ba57c
IvanaDjikic <ivdjikic@gmail.com>
2019-07-15 01:44:58 +0200

    palindrome change

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -9,4 +9,33 @@ public class Palindrome {
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+
+	public boolean wordIsInDictionary(String word) {
+		List dictionary = new Palindrome().loadWords();
+		if (dictionary.contains(word)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public String reverseWord(String word) {
+		char[] arrayOfChars = word.toCharArray();
+		char[] reversedArray = new char[word.length()];
+		for (int i = 0; i < arrayOfChars.length; i++) {
+			reversedArray[word.length() - 1 - i] = arrayOfChars[i];
+
+		}
+		String reversedWord = new String(reversedArray);
+		return reversedWord;
+	}
+
+	public boolean isPalindrome(String word) {
+		if (new Palindrome().reverseWord(word).equals(word)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
